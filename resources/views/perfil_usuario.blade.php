fb931d1acbd7701d49c4a5f6dc2f32dfea672e7e
tmp00076 <AnaCristina@Anas-MacBook-Pro-3.local>
2017-11-10 11:51:58 -0500

    Agregado icono edit

--- resources/views/perfil_usuario.blade.php
+++ resources/views/perfil_usuario.blade.php
@@ -10,13 +10,34 @@
 
       @if (!Auth::guest())
 
+  <div class="col-md-8 col-md-offset-2">
 <div class="jumbotron">
 
       <center>
       <h2><b>Perfil {{$user->name}}</b></h2>
       </center>
 
-</div>
+        <table class="table" >
+        <tbody id="perfil_usuario">
+
+          
+        
+          <tr>
+          <td><h4>Correo Institucional</h4></td>
+          <td align='right'><h4>{{$user->email}}</h4></td>
+          </tr>
+          <tr>
+          <td><h4>Departamento</h4></td>
+          <td align='right'><h4>{{$user->departamento}}</h4></td>
+          </tr>
+        
+          
+
+        </tbody>
+               </table>
+
+      </div>
+      </div>
 
            @else
 
