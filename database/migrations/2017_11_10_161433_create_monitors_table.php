fc0c2aa5c4c4f90e42e2aae418b62f9736f891fa
FelipeJB <felipejbq@hotmail.com>
2017-11-10 11:16:50 -0500

    Creacion modelo de monitor

---
+++ database/migrations/2017_11_10_161433_create_monitors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMonitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monitors', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('nombre');
+          $table->string('departamento');
+          $table->binary('imagen') ;
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monitors');
+    }
+}
