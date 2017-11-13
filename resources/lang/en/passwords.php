71e6503605ca029e6dba329cec07f150e6a7d3c0
FelipeJB <felipejbq@hotmail.com>
2017-11-12 19:29:33 -0500

    Modificadas vistas y manejo de excepciones

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Las contraseñas deben ser de mínimo 6 caracteres y deben coincidir.',
+    'reset' => 'Tu contraseña se ha restablecido!',
+    'sent' => 'Te hemos enviado a tu correo institucional las instrucciones de recuperación.',
+    'token' => 'Este token de restablecimiento es inválido.',
+    'user' => "No pudimos encontrar un usuario con esta dirección de correo electrónico.",
 
 ];
